6d553ff0acd9dd895bea64dcef49a8f24fad0d8e
Jake Moore <jakemoore@Jakes-MacBook-Air.local>
2017-06-16 00:57:18 -0400

    Finished controller actions for blog and projects. Updated live ticker on homepage so visitirs can see the latest news updated to the website.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -22,12 +22,23 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   end
 
-  def update
+  def edit
+    @project = Project.find(params[:id])
+  end
 
+  def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(project_params)
+      redirect_to project_path(@project)
+    else
+      render action: "new"
+    end
   end
 
   def destroy
-
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path
   end
 
   private
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,3 @@
 class Project < ApplicationRecord
+  validates :title, :description, :content, presence: true
 end
