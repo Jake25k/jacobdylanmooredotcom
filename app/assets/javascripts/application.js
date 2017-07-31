0d01a05605f348daffe7ce202cb72edcc97cf7f9
Jake Moore <jakemoore@Jakes-Air.ndc.nasa.gov>
2017-07-31 11:12:24 -0400

    Spellcheck for tinymce

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,11 +13,11 @@ $(document).on('turbolinks:load', function() {
     tinymce.init({
         selector: 'textarea',
         theme: "modern",
-        toolbar: "bold,italic,underline,|,bullist,numlist,outdent,indent,|,undo,redo,|,pastetext,pasteword,selectall,|,uploadimage",
-        pagebreak_separator: "<p class='page-separator'>&nbsp;</p>",
+        toolbar: "bold,italic,underline,|,bullist,numlist,outdent,indent,|,undo,redo,| selectall |,uploadimage",
         plugins: ["uploadimage"],
         relative_urls: false,
         remove_script_host: false,
+        browser_spellcheck: true,
         document_base_url: (!window.location.origin ? window.location.protocol + "//" + window.location.host : window.location.origin) + "/",
     });
 });
