24d60838ca23b15a9d701e4e06f7921385639f24
Jake Moore <jakemoore@Jakes-MacBook-Air.local>
2017-08-09 03:16:55 -0400

    Projects sliding bug

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -9,12 +9,13 @@ $(document).on('turbolinks:load', function() {
     $("#selector").change(function () {
         $(".tableToDissapear").slideUp();
         $('#renderedTable').slideUp();
+
         choice = $('select[id=selector]').val();
         if (choice != "nil") {
             $.post('projects/render/chosenProjects', {project_type: choice}, function (partial) {
                 setTimeout(function() { $('#renderedTable').html(partial); }, 400);
             })
-            $('#renderedTable').slideDown();
+            setTimeout(function() { $('#renderedTable').slideDown(); }, 400);
         }
         else {
             $('#renderedTable').slideUp();
