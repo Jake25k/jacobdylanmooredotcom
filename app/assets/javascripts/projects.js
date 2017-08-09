f4f6fa1e4a3a434440a3639ba4e813dabb5d7882
Jake Moore <jakemoore@Jakes-MacBook-Air.local>
2017-08-09 02:48:40 -0400

    Projects sliding bug

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -13,6 +13,7 @@ $(document).on('turbolinks:load', function() {
                 $('#renderedTable').html(partial)
             })
             $(".tableToDissapear").slideUp()
+            $('#renderedTable').slideUp();
             $('#renderedTable').slideDown();
         }
         else {
